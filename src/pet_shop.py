0f55d3fb08eb3dd10eac920f3e2426ec62bae2f7
Pete <peterwbrodie@gmail.com>
2021-02-28 14:11:25 +0000

    Test Increase Pets - Pass

--- src/pet_shop.py
+++ src/pet_shop.py
@@ -8,7 +8,18 @@ def get_total_cash(pet_shop):
 def add_or_remove_cash(pet_shop, amount):
     pet_shop["admin"]["total_cash"] += amount
 
+def add_or_remove_cash(pet_shop, amount):
+    pet_shop["admin"]["total_cash"] += amount
+
 def get_pets_sold(pets_sold):
     return pets_sold["admin"]["pets_sold"]
 
+def increase_pets_sold(pets_sold, sold):
+    pets_sold["admin"]["pets_sold"] += sold
+    
 
+
+# def test_increase_pets_sold(self):
+#         increase_pets_sold(self.cc_pet_shop,2)
+#         sold = get_pets_sold(self.cc_pet_shop)
+#         self.assertEqual(2, sold)
